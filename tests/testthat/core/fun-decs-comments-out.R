025c88fec2862c186b43ca57c77e19de40732697
Lorenz <lorenz.walthert@icloud.com>
2021-04-08 21:51:10 +0200

    re-add tests

---
+++ tests/testthat/core/fun-decs-comments-out.R
@@ -0,0 +1,24 @@
+
+f <- function(a,
+  b,
+  c) { # #nolint
+  just(one)
+}
+
+
+f <- function(a,
+  b,
+  c) # #nolint
+{
+
+  f()
+  g()
+  f()
+  g()
+  f()
+  g()
+  f()
+  g()
+  f()
+  g()
+}
--- tests/testthat/test-core.R
+++ tests/testthat/test-core.R
@@ -8,4 +8,7 @@ test_that("line break for multi-line function declaration", {
   expect_warning(styler:::test_collection("core", "eq-sub-replacement",
     transformer = style_text,
   ), NA)
+  expect_warning(styler:::test_collection("core", "fun-decs-comments",
+    transformer = style_text,
+  ), NA)
 })
