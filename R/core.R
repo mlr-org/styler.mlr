1914b8f66674f38f5737e92e739942032ff882ca
Lorenz <lorenz.walthert@icloud.com>
2021-04-09 14:41:23 +0200

    random fixes

--- R/core.R
+++ R/core.R
@@ -175,7 +175,7 @@ mlr_style = function(scope = "tokens",
     # transformer options
     use_raw_indention = use_raw_indention,
     reindention = reindention,
-    style_guide_name = "styler::mlr_style@https://github.com/pat-s/styler/tree/mlr-style", # TODO udpate this
+    style_guide_name = "styler.mlr::mlr_style@https://github.com/mlr-org/styler.mlr/",
     style_guide_version = version
   )
 }
